9ef2103b0a58a82725f5c11965df959f8e9d2423
Galina Marakina <g.marakina.2gis@gmail.com>
2020-06-04 12:47:35 +0300

    Add Classes

--- app/src/main/java/com/example/tutorialshow/IntroActivity.java
+++ app/src/main/java/com/example/tutorialshow/IntroActivity.java
@@ -1,13 +1,28 @@
 package com.example.tutorialshow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+
+    List<ScreenItem> mList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
+
     }
 }
