a223fc80d910e63ba818a835bd214f9d970cc2e3
Galina Marakina <g.marakina.2gis@gmail.com>
2020-06-04 11:22:17 +0300

    Add activity

--- app/src/main/java/com/example/tutorialshow/IntroActivity.java
+++ app/src/main/java/com/example/tutorialshow/IntroActivity.java
@@ -1,4 +1,13 @@
 package com.example.tutorialshow;
 
-public class IntroActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
 }
